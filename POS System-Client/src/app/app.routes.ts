3ba641c123f1f5f1206d28970bf34dc06a618365
Osagani31 <naonperera@students.nsbm.ac.lk>
2026-07-14 12:31:22 +0530

    feat : Project initialization

---
+++ POS System-Client/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import {LoginComponent} from './pages/login/login.component';
+import {NotfoundComponent}  from './pages/notfound/notfound.component';
+
+export const routes: Routes = [
+  {path:'', redirectTo: '/login' , pathMatch:'full'},
+  {path:'login',component: LoginComponent},
+  {path:'**', component:NotfoundComponent},
+
+];

b81fa37b2346d2231aceb38bdc35f676102c1c31
Osagani31 <naonperera@students.nsbm.ac.lk>
2026-07-15 15:56:40 +0530

    Delete POS System-Client/src/app directory

--- POS System-Client/src/app/pages/notfound/notfound.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-notfound.component',
-  imports: [],
-  templateUrl: './notfound.component.html',
-  styleUrl: './notfound.component.scss',
-})
-export class NotfoundComponent {}
